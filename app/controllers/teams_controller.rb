255f1b0299f5b0fde3e70ab6ca12d9fd79df5cb6
Adrian PK <adrianpk@adrianpk.com>
2025-05-13 23:02:02 +0200

    Implement teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -31,6 +31,35 @@ class TeamsController < ApplicationController
     end
   end
 
+  def members
+    @team = Team.find(params[:id])
+    @members = @team.users
+    @available_users = User.where.not(id: @members.pluck(:id))
+  end
+
+  def add_member
+    @team = Team.find(params[:id])
+    user = User.find(params[:user_id])
+    unless @team.users.exists?(user.id)
+      @team.team_memberships.create(id: SecureRandom.uuid, user: user)
+      redirect_to members_team_path(@team), notice: "User #{user.username} added to the team."
+    else
+      redirect_to members_team_path(@team), alert: "User #{user.username} is already a member of the team."
+    end
+  end
+
+  def remove_member
+    @team = Team.find(params[:id])
+    user = User.find(params[:user_id])
+    membership = @team.team_memberships.find_by(user: user)
+    if membership
+      membership.destroy
+      redirect_to members_team_path(@team), notice: "User #{user.username} removed from the team."
+    else
+      redirect_to members_team_path(@team), alert: "User #{user.username} is not a member of the team."
+    end
+  end
+
   private
 
   def team_params
