0b8b2a05492bd2eb384b27a9786f618c93e479e9
Adrian PK <adrianpk@adrianpk.com>
2025-06-01 13:42:02 +0200

    Make daily jobs honor weekday flags

--- app/jobs/daily_reminder_job.rb
+++ app/jobs/daily_reminder_job.rb
@@ -5,6 +5,15 @@ class DailyReminderJob < ApplicationJob
     job = Job.find(job_id)
     job.update!(state: "processing")
     daily_setup = DailySetup.find(job.target_id)
+
+    current_day_method_name = Time.current.strftime("%A").downcase.to_sym
+
+    unless daily_setup.send(current_day_method_name)
+      Rails.logger.info "[DailyReminderJob] Skipped for daily ##{daily_setup.id} (Job ##{job.id}). Day '#{current_day_method_name}' is not active."
+      job.update!(state: "completed", executed_at: Time.current)
+      return
+    end
+
     team = daily_setup.team
     team.users.each do |user|
       DailyReminderMailer.reminder_email(user, daily_setup).deliver_later
--- app/jobs/daily_report_job.rb
+++ app/jobs/daily_report_job.rb
@@ -2,6 +2,20 @@ class DailyReportJob < ApplicationJob
   queue_as :default
 
   def perform(daily_setup_id)
+    daily_setup = DailySetup.find_by(id: daily_setup_id) # Changed from find to find_by
+
+    unless daily_setup # Added check for nil
+      Rails.logger.warn "[DailyReportJob] DailySetup with ID ##{daily_setup_id} not found. Skipping job."
+      return
+    end
+
+    current_day_method_name = Time.current.strftime("%A").downcase.to_sym # :monday, :tuesday, etc.
+
+    unless daily_setup.send(current_day_method_name)
+      Rails.logger.info "[DailyReportJob] Skipped for DailySetup ##{daily_setup.id}. Day '#{current_day_method_name}' is not active."
+      return
+    end
+
     Rails.logger.info "[DailyReportJob] Would publish daily report for DailySetup ##{daily_setup_id} (placeholder)"
   end
 end
